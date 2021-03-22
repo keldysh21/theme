7c0b0b771d34e6bae3888ed28d3f5df09f665d11
keldysh21 <keldysh21@example.com>
2021-03-22 21:57:47 +0000

    initial commit

--- theme/hooks.py
+++ theme/hooks.py
@@ -15,11 +15,11 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/theme/css/theme.css"
+app_include_css = "/assets/theme/css/theme.css"
 # app_include_js = "/assets/theme/js/theme.js"
 
 # include js, css files in header of web template
-# web_include_css = "/assets/theme/css/theme.css"
+web_include_css = "/assets/theme/css/theme.css"
 # web_include_js = "/assets/theme/js/theme.js"
 
 # include custom scss in every website theme (without file extension ".scss")
